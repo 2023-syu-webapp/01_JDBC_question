4c60f839c894265623b765e99f5169586bf45894
DESKTOP-OMBAS04\anty3 <anty3125@naver.com>
2023-05-25 00:14:33 +0900

    [실습문제 풀이완료]
    이주성 실습문제제출입니다.

--- jdbc-practice-source/src/main/java/run/deleteEmployeeInfo.java
+++ jdbc-practice-source/src/main/java/run/deleteEmployeeInfo.java
@@ -1,5 +1,19 @@
 package run;
 
+import model.dto.EmployeeDTO;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Scanner;
+
+import static common.JDBCTemplate.close;
+import static common.JDBCTemplate.getConnection;
+
 public class deleteEmployeeInfo {
 
     // 삭제할 사원의 이름을 사용자로부터 입력받아 삭제
@@ -7,4 +21,43 @@ public class deleteEmployeeInfo {
     // 사원 삭제에 성공하면 "[이름]님이 퇴출되었습니다." 출력
     // 사원 삭제에 실패하면 "[이름]님은 퇴출될 수 없습니다." 출력
 
+    public static void main(String[] args) {
+        Connection con = getConnection();
+
+        PreparedStatement pstmt = null;
+        int result = 0;
+
+        Properties prop = new Properties();
+
+        EmployeeDTO row = null;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("삭제할 사원의 이름을 입력하세요 : ");
+        String name = sc.nextLine();
+
+        try {
+            prop.loadFromXML(new FileInputStream("jdbc-practice-source/src/main/java/mapper/employee-query.xml"));
+            String query = prop.getProperty("deleteMenu");
+
+            row = new EmployeeDTO();
+            row.setEmpName(name);
+
+            pstmt = con.prepareStatement(query);
+            pstmt.setString(1, row.getEmpName());
+
+            result = pstmt.executeUpdate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(con);
+            close(pstmt);
+        }
+        if(result > 0){
+            System.out.printf("[%s]님이 퇴출되었습니다.", name);
+        }else {
+            System.out.printf("[%s]님은 퇴출될 수 없습니다.", name);
+        }
+    }
 }
--- jdbc-practice-source/src/main/java/run/updateEmployeeInfo.java
+++ jdbc-practice-source/src/main/java/run/updateEmployeeInfo.java
@@ -1,5 +1,18 @@
 package run;
 
+import model.dto.EmployeeDTO;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Scanner;
+
+import static common.JDBCTemplate.getConnection;
+import static common.JDBCTemplate.close;
+
 public class updateEmployeeInfo {
 
     // 수정할 사원 번호를 입력받고
@@ -7,4 +20,63 @@ public class updateEmployeeInfo {
     // update 성공하면 "직원 정보 수정에 성공하였습니다." 출력
     // update 실패하면 "직원 정보 수정에 실패하였습니다." 출력
 
+    public static void main(String[] args) {
+        Connection con = getConnection();
+
+        PreparedStatement pstmt = null;
+        int result = 0;
+
+        Properties prop = new Properties();
+
+        EmployeeDTO row = null;
+
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("수정할 사원 번호를 입력하세요 : ");
+        String empId = sc.nextLine();
+        System.out.print("변경할 전화번호를 입력하세요 : ");
+        String phone = sc.nextLine();
+        System.out.print("변경할 이메일을 입력하세요 : ");
+        String email = sc.nextLine();
+        System.out.print("변경할 부서코드를 입력하세요 : ");
+        String deptCode = sc.nextLine();
+        System.out.print("변경할 급여를 입력하세요 : ");
+        int salary = sc.nextInt();
+        System.out.print("변경할 보너스를 입력하세요 : ");
+        double bonus = sc.nextDouble();
+
+        try {
+            prop.loadFromXML(new FileInputStream("jdbc-practice-source/src/main/java/mapper/employee-query.xml"));
+            String query = prop.getProperty("updateMenu");
+
+            row = new EmployeeDTO();
+            row.setEmpId(empId);
+            row.setPhone(phone);
+            row.setEmail(email);
+            row.setDeptCode(deptCode);
+            row.setSalary(salary);
+            row.setBonus(bonus);
+
+            pstmt = con.prepareStatement(query);
+            pstmt.setString(1, row.getPhone());
+            pstmt.setString(2, row.getEmail());
+            pstmt.setString(3, row.getDeptCode());
+            pstmt.setInt(4, row.getSalary());
+            pstmt.setDouble(5, row.getBonus());
+            pstmt.setString(6, row.getEmpId());
+
+            result = pstmt.executeUpdate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(con);
+            close(pstmt);
+        }
+        if(result > 0){
+            System.out.println("직원 정보 수정에 성공하였습니다.");
+        }else {
+            System.out.println("직원 정보 수정에 실패하였습니다.");
+        }
+    }
 }
